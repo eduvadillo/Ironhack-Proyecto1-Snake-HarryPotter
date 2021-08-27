fc85d152eb063e2f7042f2af5b03d8847550d12f
eduvadillo <87086693+eduvadillo@users.noreply.github.com>
2021-08-27 13:38:45 +0200

    cambiar teclas para mejorar experienca del juego

--- js/indexMultiplayerPrueba.js
+++ js/indexMultiplayerPrueba.js
@@ -310,27 +310,27 @@ barraDiferencia.appendChild(nuevoDiv2);
 
     function keyDown(event) {
       //arriba
-      if (event.keyCode == 38) {
+      if (event.keyCode == 87) {
         if (yVelocity == 1) return;
         yVelocity = -1;
         xVelocity = 0;
       }
       //abajo
-      if (event.keyCode == 40) {
+      if (event.keyCode == 83) {
         if (yVelocity == -1) return;
         yVelocity = 1;
         xVelocity = 0;
       }
 
       //izquierda
-      if (event.keyCode == 37) {
+      if (event.keyCode == 65) {
         if (xVelocity == 1) return;
         yVelocity = 0;
         xVelocity = -1;
       }
 
       // derecha
-      if (event.keyCode == 39) {
+      if (event.keyCode == 68) {
         if (xVelocity == -1) return;
         yVelocity = 0;
         xVelocity = 1;
@@ -528,27 +528,27 @@ barraDiferencia.appendChild(nuevoDiv2);
 
     function keyDown2(event) {
       //arriba
-      if (event.keyCode == 87) {
+      if (event.keyCode == 38) {
         if (yVelocity2 == 1) return;
         yVelocity2 = -1;
         xVelocity2 = 0;
       }
       //abajo
-      if (event.keyCode == 83) {
+      if (event.keyCode == 40) {
         if (yVelocity2 == -1) return;
         yVelocity2 = 1;
         xVelocity2 = 0;
       }
 
       //izquierda
-      if (event.keyCode == 65) {
+      if (event.keyCode == 37) {
         if (xVelocity2 == 1) return;
         yVelocity2 = 0;
         xVelocity2 = -1;
       }
 
       // derecha
-      if (event.keyCode == 68) {
+      if (event.keyCode == 39) {
         if (xVelocity2 == -1) return;
         yVelocity2 = 0;
         xVelocity2 = 1;
